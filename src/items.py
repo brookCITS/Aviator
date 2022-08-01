cac146670f8fedf7d4e8d3d328d2909b15a73481
Vina Chen <cvina2021@gmail.com>
2022-08-01 12:54:51 -0400

    done

--- src/items.py
+++ src/items.py
@@ -10,4 +10,14 @@ class Plane(arcade.Sprite):
         super().__init__(filename, sprite_scaling)
 
     def update(self):
-        
+
+class MiniBossCloud(arcade.Sprite):
+
+    def __init__(self, filename, sprite_scaling, x, y, speed):
+        super().__init__(filename, sprite_scaling)
+        self.x = 0
+        self.y = 0
+        speed = random.randint(1,10)
+
+    def update(self):
+        self.x += speed
