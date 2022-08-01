47389dca848e03c496a29d21529811f4764dbc81
Vina Chen <cvina2021@gmail.com>
2022-08-01 12:51:15 -0400

    done

--- src/items.py
+++ src/items.py
@@ -10,4 +10,14 @@ class Plane(arcade.Sprite):
         super().__init__(filename, sprite_scaling)
 
     def update(self):
-        
+
+class Cloud(arcade.Sprite):
+
+    def __init__(self, filename, ,sprite_scaling, x, y, speed):
+        super().__init__(filename, sprite_scaling)
+        self.x = 0
+        self.y = 0
+        speed = random.randint(0,10)
+
+    def update(self, speed):
+        self.x += self.speed
