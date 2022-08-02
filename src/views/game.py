88678ed8b2a15deab4980467044053485a530ef5
KhayaG122 <108814316+KhayaG122@users.noreply.github.com>
2022-08-02 14:43:31 -0400

    done

--- src/views/game.py
+++ src/views/game.py
@@ -22,7 +22,43 @@ class GameView(arcade.View):
 
 
     def on_draw(self):
+        filename = "src/images/"+self.options["color"]+".png"
+      self.player=Plane(filename, 0.3)
+      self.clouds = arcade.SpriteList()
+      self.clouds_miniboss = arcade.SpriteList()
 
+      if self.option["level"] == "beginner":
+        speed = 3
+        #5 regular clouds (speed slow)
+        for cloud in range(5):
+          self.clouds.append(Cloud("src/images/cloud.png",0.2, speed))
+          #2 miniboss 
+        for cloud in range(2):
+            self.clouds_miniboss.append(MiniBossCloud("src/images/miniboss.png",0.2, speed))
+          
+          self.clouds_boss = BossCloud("src/images/miniboss.png", 0.5, speed)
+          
+        
+      elif self.option["level"] == "intermidiate":
+        speed = 4
+        #10 regular clouds (speed slow)
+        for cloud in range(10):
+          self.clouds.append(Cloud("src/images/cloud.png",0.2, speed))
+          #2 miniboss 
+        for cloud in range(4):
+            self.clouds_miniboss.append(MiniBossCloud("src/images/miniboss.png",0.2, speed))
+        self.clouds_boss = BossCloud("src/images/miniboss.png", 0.5, speed)
+        
+      elif self.option["level"] == "advanced":
+        speed = 5
+        #15 regular clouds (speed slow)
+        for cloud in range(15):
+          self.clouds.append(Cloud("src/images/cloud.png",0.2, speed))
+          #2 miniboss 
+        for cloud in range(6):
+            self.clouds_miniboss.append(MiniBossCloud("src/images/miniboss.png",0.2, speed))
+          
+        self.clouds_boss = BossCloud("src/images/miniboss.png", 0.5, speed)
 
     def on_key_press(self, symbol,modifier):
 
