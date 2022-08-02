40adc319fa2a8e08f09dd410d5d19817decb4774
Quadaire <109605760+Quadaire@users.noreply.github.com>
2022-08-02 13:10:54 -0400

    done

--- src/views/game.py
+++ src/views/game.py
@@ -25,8 +25,12 @@ class GameView(arcade.View):
 
 
     def on_key_press(self, symbol,modifier):
-
-
+ if symbol == arcade.key.RIGHT:
+            print("Right arrow key is pressed")
+            self.player.change_x = 4
+      if symbol == arcade.key.LEFT:
+            print("Left arrow key is pressed")
+            self.player.change_x = -4 
     def on_key_release(self, key, modifiers):
 
 
