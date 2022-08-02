1cfa20b83f7a76eef3de2f524c40f06b4b3ed0ba
DarrienCunningham <100631754+DarrienCunningham@users.noreply.github.com>
2022-08-02 14:49:20 -0400

    not done

--- src/views/gameover.py
+++ src/views/gameover.py
@@ -9,17 +9,11 @@ import menu
 class GameOverView(arcade.View):
     def __init__(self):
         super().__init__()
-        self.options = {
-            'color': 'green', 'white', 'blue', 'yellow'
-            'level': 'beginner', 'intermediate', 'advanced'
-        }
 
 
     def on_show_view(self):
-        return super().on_show_view()
+        
 
 
     def on_draw(self):
-        """Draw this view"""
-        self.clear()
-        self.manager.draw()
+        
