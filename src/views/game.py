ca75f30dbf1f72ae12c711c105f379b22fe01ca3
brookCITS <brook@codeintheschools.org>
2022-08-02 14:13:29 -0400

    updated sytanx

--- src/views/game.py
+++ src/views/game.py
@@ -18,7 +18,6 @@ class GameView(arcade.View):
         """ Initializer """
         # Call the parent class initializer
         super().__init__()
-<<<<<<< HEAD
         self.options=options
 
         self.player=None
@@ -30,9 +29,6 @@ class GameView(arcade.View):
         self.set_mouse_visible(False)
         self.score=0
         self.lives=3
-=======
-
->>>>>>> AVA-22
 
 
     def on_draw(self):
@@ -45,13 +41,13 @@ class GameView(arcade.View):
         if symbol == arcade.key.LEFT:
             print("Left arrow key is pressed")
             self.player.change_x = -4
-            
+
     def on_key_release(self, key, modifiers):
         if symbol == arcade.key.RIGHT:
             print("Right arrow key is pressed")
             self.player.change_x = 0
         if symbol == arcade.key.LEFT:
             print("Left arrow key is pressed")
-            self.player.change_x = 0 
+            self.player.change_x = 0
 
     def update(self, delta_time):
