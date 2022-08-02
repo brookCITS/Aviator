4c1e6fd9a6b06c3b50b59f03e19ced772c8ee44e
Samuel Dixler <dixler.inc@gmail.com>
2022-08-02 14:07:13 -0400

    probably done but need someone to design game logo

--- src/views/menu.py
+++ src/views/menu.py
@@ -31,35 +31,34 @@ class MenuView(arcade.View):
         self.h_box3.vertical = False
 
 
-#DISPLAYABLES
+#DISPLAY
 #------------------
-
-#GAME TITLE
+    #GAME TITLE
         title_label = arcade.gui.UITextArea(text="Aviator", text_color=arcade.color.RED, font_size=60, font_name="FFF Tusj")
         title_label.fit_content()
 
-#COLOR CHOOSE
+    #COLOR CHOOSE
         color_choose_text = arcade.gui.UILabel(text='CHOOSE A COLOR')
         color_choose_text.fit_content()
 
     #COLORS
-        green = arcade.gui.UITextureButton(x=0, y=0, texture=arcade.load_texture('src/images/green.png'), texture_hovered=arcade.load_texture('src/images/green_clicked.png'), scale=2)
-        blue = arcade.gui.UITextureButton(x=0, y=0, texture=arcade.load_texture('src/images/blue.png'), texture_hovered=arcade.load_texture('src/images/blue_clicked.png'), scale=2)
-        white = arcade.gui.UITextureButton(x=0, y=0, texture=arcade.load_texture('src/images/white.png'), texture_hovered=arcade.load_texture('src/images/white_clicked.png'), scale=2)
-        yellow = arcade.gui.UITextureButton(x=0, y=0, texture=arcade.load_texture('src/images/yellow.png'), texture_hovered=arcade.load_texture('src/images/yellow_clicked.png'), scale=2)
+        green = arcade.gui.UITextureButton(x=0, y=0, texture=arcade.load_texture('src/images/planes/green.png'), texture_hovered=arcade.load_texture('src/images/planes/green_clicked.png'), scale=2)
+        blue = arcade.gui.UITextureButton(x=0, y=0, texture=arcade.load_texture('src/images/planes/blue.png'), texture_hovered=arcade.load_texture('src/images/planes/blue_clicked.png'), scale=2)
+        white = arcade.gui.UITextureButton(x=0, y=0, texture=arcade.load_texture('src/images/planes/white.png'), texture_hovered=arcade.load_texture('src/images/planes/white_clicked.png'), scale=2)
+        yellow = arcade.gui.UITextureButton(x=0, y=0, texture=arcade.load_texture('src/images/planes/yellow.png'), texture_hovered=arcade.load_texture('src/images/planes/yellow_clicked.png'), scale=2)
 
-#LEVEL CHOOSE
+    #LEVEL CHOOSE
         level_choose_text = arcade.gui.UILabel(text='CHOOSE A DIFFICULTY LEVEL')
-        level_beginner = arcade.gui.UITextureButton(texture=arcade.load_texture('src/images/beginner.png'), texture_hovered=arcade.load_texture('src/images/beginner_clicked.png'), scale=.8)
-        level_intermediate = arcade.gui.UITextureButton(texture=arcade.load_texture('src/images/intermediate.png'), texture_hovered=arcade.load_texture('src/images/intermediate_clicked.png'), scale=.8)
-        level_expert = arcade.gui.UITextureButton(texture=arcade.load_texture('src/images/expert.png'), texture_hovered=arcade.load_texture('src/images/expert_clicked.png'), scale=.8)
+        level_beginner = arcade.gui.UITextureButton(texture=arcade.load_texture('src/images/buttons/beginner.png'), texture_hovered=arcade.load_texture('src/images/buttons/beginner_clicked.png'), scale=.8)
+        level_intermediate = arcade.gui.UITextureButton(texture=arcade.load_texture('src/images/buttons/intermediate.png'), texture_hovered=arcade.load_texture('src/images/buttons/intermediate_clicked.png'), scale=.8)
+        level_expert = arcade.gui.UITextureButton(texture=arcade.load_texture('src/images/buttons/expert.png'), texture_hovered=arcade.load_texture('src/images/buttons/expert_clicked.png'), scale=.8)
 
-#FINAL CHOICE
+    #FINAL CHOICE
         color_choice = arcade.gui.UILabel(text="GREEN", width=150, align='center')
         level_choice = arcade.gui.UILabel(text="BEGINNER", width=150, align='center')
 
-#START
-        start_game = arcade.gui.UITextureButton(texture= arcade.load_texture('src\images\start_g.png'), texture_hovered=arcade.load_texture('src/images/start_green_clicked.png'), scale=.7)
+    #START
+        start_game = arcade.gui.UITextureButton(texture= arcade.load_texture('src/images/buttons/start_g.png'), texture_hovered=arcade.load_texture('src/images/buttons/start_green_clicked.png'), scale=.7)
 
 #CODE THAT ADDS TO DISPLAY
 #-------------------------
@@ -103,29 +102,29 @@ class MenuView(arcade.View):
         def on_click_flatbutton(event):
             self.options['color']='green'
             color_choice.text="GREEN"
-            start_game.texture= arcade.load_texture('src\images\start_g.png')
-            start_game.texture_hovered= arcade.load_texture('src/images/start_green_clicked.png')
+            start_game.texture= arcade.load_texture('src/images/buttons/start_g.png')
+            start_game.texture_hovered= arcade.load_texture('src/images/buttons/start_green_clicked.png')
 
         @blue.event("on_click")
         def on_click_flatbutton(event):
             self.options['color']='blue'
             color_choice.text="BLUE"
-            start_game.texture= arcade.load_texture('src\images\start_b.png')
-            start_game.texture_hovered= arcade.load_texture('src/images/start_blue_clicked.png')
+            start_game.texture= arcade.load_texture('src/images/buttons/start_b.png')
+            start_game.texture_hovered= arcade.load_texture('src/images/buttons/start_blue_clicked.png')
 
         @white.event("on_click")
         def on_click_flatbutton(event):
             self.options['color']='white'
             color_choice.text="WHITE"
-            start_game.texture= arcade.load_texture('src\images\start_w.png')
-            start_game.texture_hovered= arcade.load_texture('src/images/start_white_clicked.png')
+            start_game.texture= arcade.load_texture('src/images/buttons/start_w.png')
+            start_game.texture_hovered= arcade.load_texture('src/images/buttons/start_white_clicked.png')
 
         @yellow.event("on_click")
         def on_click_flatbutton(event):
             self.options['color']='yellow'
             color_choice.text="YELLOW"
-            start_game.texture= arcade.load_texture('src\images\start_y.png')
-            start_game.texture_hovered= arcade.load_texture('src/images/start_yellow_clicked.png')
+            start_game.texture= arcade.load_texture('src/images/buttons/start_y.png')
+            start_game.texture_hovered= arcade.load_texture('src/images/buttons/start_yellow_clicked.png')
 
         @level_beginner.event("on_click")
         def on_click_flatbutton(event):
