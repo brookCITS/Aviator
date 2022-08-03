389d81147bfb8c3d499982a6654f09d3aad5e7d5
DarrienCunningham <100631754+DarrienCunningham@users.noreply.github.com>
2022-08-03 12:40:40 -0400

    Update gameover.py

--- src/views/gameover.py
+++ src/views/gameover.py
@@ -9,11 +9,48 @@ import menu
 class GameOverView(arcade.View):
     def __init__(self):
         super().__init__()
+      self.options = {
+            'color': 'green', 'white', 'blue', 'yellow'
+            'level': 'beginner', 'intermediate', 'advanced'
+        }   
 
 
     def on_show_view(self):
-        
+     return super().on_show_view()
 
 
     def on_draw(self):
+        """ Draw this view """
+        self.clear()
+        self.manager.draw()
         
+   @greenbutton.event("on_click")
+   def on_click_flatbutton(event):
+        self.options['color'] = 'green'
+        
+   @whitebutton.event("on_click")
+   def on_click_flatbutton(event):
+        self.options['color'] = 'white
+        
+    @bluebutton.event("on_click")
+   def on_click_flatbutton(event):
+        self.options['color'] = 'blue'
+        
+    @yellowbutton.event("on_click")
+   def on_click_flatbutton(event):
+        self.options['color'] = 'yellow'
+        
+    @beginnerbutton.event("on_click")
+   def on_click_flatbutton(event):
+        self.options['level'] = 'beginner'
+        
+   @intermediatebutton.event("on_click")
+   def on_click_flatbutton(event):
+        self.options['level'] = 'intermediate'
+        
+   @advancedbutton.event("on_click")
+   def on_click_flatbutton(event):
+        self.options['level'] = 'advanced'
+        
+    
+    
